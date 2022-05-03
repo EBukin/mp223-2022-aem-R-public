26f10d9edb94a485711ded9f668b231771481b7f
Eduard Bukin <edwardbukin@gmail.com>
2022-05-03 16:29:48 +0200

    Making simple regression slides and HW

---
+++ ae/ae04-multiple-regression-part-1/ae04-01-simple-regression-from-slides.R
@@ -0,0 +1,31 @@
+
+# This is an R script, where we will practice running 
+#   R code without using an RMarkdown document.
+
+# What you need to do here, is to run all the code form the 
+#   W04-00 Simple regression slides in here as the R code
+
+
+# Setup
+
+
+# Data loading
+
+
+# Scatter plot
+
+
+# Simple linear regression
+
+
+# Regression summary: summary
+
+
+# Regression summary: broom
+
+
+# Regression summary: performance and parameters
+
+
+# Residuals vs Fitter plot
+
--- ae/ae04-regression/ae04-01-simple-regression-from-slides.R
+++
@@ -1,7 +0,0 @@
-
-# This is an R script, where we will practice running 
-#   R code without building a document.
-# Follow video instruction on making R Script out of the slides.
-
-
-
